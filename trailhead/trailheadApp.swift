40e00bf99f018f05ab3c48cda9e21400725ceed8
robingotz <tug29225@temple.edu>
2024-12-11 22:27:41 +0100

    Initial Commit

---
+++ trailhead/trailheadApp.swift
@@ -0,0 +1,17 @@
+//
+//  trailheadApp.swift
+//  trailhead
+//
+//  Created by Robin Götz on 12/11/24.
+//
+
+import SwiftUI
+
+@main
+struct trailheadApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ trailheadTests/trailheadTests.swift
@@ -0,0 +1,16 @@
+//
+//  trailheadTests.swift
+//  trailheadTests
+//
+//  Created by Robin Götz on 12/11/24.
+//
+
+import Testing
+
+struct trailheadTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
